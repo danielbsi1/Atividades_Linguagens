2e7e05c492e2ce606baf938262e0d0ed75183ae2
Daniel Costa Alencar <138629348+danielbsi1@users.noreply.github.com>
2023-12-07 01:05:19 -0300

    Atividade1.py

--- Atividade1.py
+++ Atividade1.py
@@ -1,20 +1,57 @@
-# ele quer nome idade sexo telefone #
-lista_f = []
 lista_m = []
-lista_nomes = []
-lista_idades = []
+lista_f = []
+lista_i = []
 lista_sexo = []
+lista_nome = []
+lista_idade = []
 lista_tel = []
 
 
 def form():
+    global nome, idade, sexo, tel
+    nome = 1
     while nome != '0':
-        nome = str(input('Ola, qual é o seu nome?? ')).capitalize()
-        sexo = str(input('Qual é o seu sexo? M/F'))
-        idade = int(input('Quantos anos você tem?? '))
-        tel = int(input('Qual é o seu número de telefone?? '))
-        arquivo = open('atv1.txt', 'a', encoding='utf-8')
-        arquivo.write(f'|Seu nome é {nome}|seu sexo é {sexo}|Sua idade é {idade}|Seu telefone é {tel}\n')
+        nome = input('Olá, qual é o seu nome? ').capitalize()
+        lista_nome.append(nome)
+        idade = input('Quantos anos você tem? ')
+        lista_idade.append(idade)
+        sexo = input('Qual é o seu sexo? M/F ').lower()
+        if sexo == 'm':
+            sexo = 'Masculino'
+            lista_m.append(nome)
+        elif sexo == 'f':
+            sexo = 'Feminino'
+            lista_f.append(nome)
+        else:
+            sexo = 'não especificado'
+        lista_sexo.append(sexo)
+        try:
+            tel = int(input('Qual é o seu número de telefone? '))
+        except ValueError:
+            tel = 'número invalido'
+        lista_tel.append(tel)
+        arquivo = open('uga.txt', 'a', encoding='utf-8')
+        arquivo.write(f'|Seu nome é {nome}|Tem {idade} anos|Seu Sexo é {sexo}|Número de telefone: {tel}|\n')
+        continuar = input('Quer adicionar outro nome? S/N').lower()
+        if continuar == 'n':
+            break
+    print('Dados armazenados')
+
+
+def lerdados():
+    print('-' * 30)
+    for c in range(0, len(lista_nome)):
+        print(f'Nome: {lista_nome[c]}\nIdade: {lista_idade[c]}\nSexo: {lista_sexo[c]}\nTelefone: {lista_tel[c]}')
+    print('-' * 30)
+
+def busca_usuario_pelo_sexo():
+    ver = input("quer ver a lista de qual sexo? M/F/ ").lower()
+    if ver == 'm':
+        print(f'O nome das pessoas do sexo masculino cadastrados são: {lista_m}')
+    elif ver == 'f':
+        print(f'O nome das pessoas de sexo feminino cadastrado são: {lista_f}')
 
 
 form()
+busca_usuario_pelo_sexo()
+lerdados()
